48a0abe2f0519bd3f141b5c03324512877b6f576
Lorenzo Belli <lorenzo.belli@upf.edu>
2025-09-16 15:06:44 +0200

    Add files via upload

---
+++ render-webpage.R
@@ -0,0 +1,24 @@
+## my web page
+
+# install.packages(c("rmarkdown", "knitr"))
+
+# show current folder and files
+getwd()
+setwd("C:/Users/u218305/OneDrive - Alma Mater Studiorum Università di Bologna/Università/Dottorato/my-webpage Markdown/lorenzo-belli.github.io-main")
+list.files()
+
+# attempt to render and show all messages
+rmarkdown::render_site()
+
+
+
+
+
+
+
+
+
+
+
+
+
